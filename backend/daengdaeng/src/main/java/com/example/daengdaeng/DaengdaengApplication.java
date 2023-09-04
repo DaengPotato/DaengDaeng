69862d029f52708f0b4480332d59977856503b15
김혜진 <zlagg23@gmail.com>
2023-09-04 16:36:50 +0900

    init: daengdaeng 프로젝트 생성

---
+++ backend/daengdaeng/src/main/java/com/example/daengdaeng/DaengdaengApplication.java
@@ -0,0 +1,13 @@
+package com.example.daengdaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaengdaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaengdaengApplication.class, args);
+	}
+
+}
---
+++ backend/daengdaeng/src/test/java/com/example/daengdaeng/DaengdaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daengdaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaengdaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
