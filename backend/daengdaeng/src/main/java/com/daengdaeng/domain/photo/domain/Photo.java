b38f2651bc13638e7ae22b63c0b93aa554d0408d
김혜진 <zlagg23@gmail.com>
2023-09-06 13:55:55 +0900

    fix: photo Entity 수정

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/photo/domain/Photo.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/photo/domain/Photo.java
@@ -1,14 +1,17 @@
 package com.daengdaeng.domain.photo.domain;
 
 import com.daengdaeng.domain.place.domain.Place;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.lang.reflect.Member;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Photo {
 
@@ -16,22 +19,25 @@ public class Photo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long photoId;
 
-    private String imageUrl;
+    @NotBlank
+    private String image;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
+    @NotNull
     private Member member;
 
 
-    @ManyToOne
-    @JoinColumn(name = "palce_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    @NotNull
     private Place place;
 
 
     @Builder
-    public Photo(Long photoId, String imageUrl, Member member, Place place) {
+    public Photo(Long photoId, String image, Member member, Place place) {
         this.photoId = photoId;
-        this.imageUrl = imageUrl;
+        this.image = image;
         this.member = member;
         this.place = place;
     }
