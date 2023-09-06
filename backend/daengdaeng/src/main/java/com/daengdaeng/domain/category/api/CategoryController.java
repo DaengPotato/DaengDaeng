13049b107bfa85b3e0bff259960c1e0c6036417d
jini11 <jieun21124@naver.com>
2023-09-06 17:07:08 +0900

    feat: category component 추가 category 관련 controller, service, repository class 추가

---
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/category/api/CategoryController.java
@@ -0,0 +1,7 @@
+package com.daengdaeng.domain.category.api;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CategoryController {
+}
---
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/category/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.daengdaeng.domain.category.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daengdaeng.domain.category.domain.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+}
