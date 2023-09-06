20e2e9761a12e316d91579b3b9e02bb43a211cfb
jini11 <jieun21124@naver.com>
2023-09-06 17:03:28 +0900

    feat: review entity 추가 review 관련 entity 추가

---
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/review/domain/Review.java
@@ -0,0 +1,51 @@
+package com.daengdaeng.domain.review.domain;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import com.daengdaeng.domain.place.domain.Place;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int reviewId;
+
+	@ColumnDefault("5")
+	@Column(nullable = false)
+	private int score;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "place_id")
+	private Place place;
+
+	@Builder
+	public Review(int reviewId, int score, Member member, Place place) {
+		this.reviewId = reviewId;
+		this.score = score;
+		this.member = member;
+		this.place = place;
+	}
+
+}
