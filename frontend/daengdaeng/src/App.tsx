f4efa8111d5d2522fdddc0c4a5870d839aebd4b1
imhynni <omelas329@gmail.com>
2023-09-08 17:58:00 +0900

    design: 뷰포트 높이 설정

--- frontend/daengdaeng/src/App.tsx
+++ frontend/daengdaeng/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/common/NavBar';
+import NavBar from './components/common/NavBar/NavBar';
 import MainPage from './pages/MainPage/indes';
 
 function App() {
+  const setScreenSize = () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
     <div className='App'>
       <NavBar />
