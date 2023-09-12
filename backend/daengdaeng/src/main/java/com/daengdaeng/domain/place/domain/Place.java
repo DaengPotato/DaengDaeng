80785c6c07f1ecab1c028f1c49b25cbe73f1613f
jini11 <jieun21124@naver.com>
2023-09-12 09:59:28 +0900

    fix: 매핑 관계 수정
    1. Place, Category 매핑 관계 수정
    2. Facility 제거

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/domain/Place.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/domain/Place.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
 import com.daengdaeng.domain.category.domain.Category;
@@ -43,7 +44,7 @@ public class Place {
 
 	private String content;
 
-	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "category_id")
 	private Category category;
 
