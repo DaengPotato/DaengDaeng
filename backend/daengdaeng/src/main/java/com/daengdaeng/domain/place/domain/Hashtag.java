36766c1400adabfa9c410a411d24f22968954e16
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-14 15:35:44 +0900

    S09P22A103-396 fix: hashtag 클래스명 재수정 (#5)

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/domain/Hashtag.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/domain/Hashtag.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class hashtag {
+public class Hashtag {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/repository/HashtagRepository.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/repository/HashtagRepository.java
@@ -3,8 +3,8 @@ package com.daengdaeng.domain.place.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.daengdaeng.domain.place.domain.hashtag;
+import com.daengdaeng.domain.place.domain.Hashtag;
 
 @Repository
-public interface HashtagRepository extends JpaRepository<hashtag, Integer> {
+public interface HashtagRepository extends JpaRepository<Hashtag, Integer> {
 }
