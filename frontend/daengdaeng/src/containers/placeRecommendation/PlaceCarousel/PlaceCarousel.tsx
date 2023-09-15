bb1513585385eb80784c60ea72ab75845693989c
imhynni <omelas329@gmail.com>
2023-09-15 17:52:15 +0900

    S09P22A103-26 feat: PlaceCard Carousel 컴포넌트 구현

---
+++ frontend/daengdaeng/src/containers/placeRecommendation/PlaceCarousel/PlaceCarousel.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+
+import useEmblaCarousel from 'embla-carousel-react';
+
+import styles from './PlaceCarousel.module.scss';
+import Card from '../../../components/common/Card/Card';
+
+import type { EmblaOptionsType } from 'embla-carousel-react';
+
+type CarouselProps = {
+  slides: React.ReactNode[];
+  options?: EmblaOptionsType;
+};
+
+const PlaceCarousel = ({ slides, options }: CarouselProps) => {
+  const [emblaRef, _] = useEmblaCarousel(options);
+
+  useEffect(() => {
+    // TODO: 장소 정보 데이터 fetch
+  });
+
+  return (
+    <div className={styles.Carousel}>
+      <div className={styles.viewport} ref={emblaRef}>
+        <div className={styles.container}>
+          {slides.map((slide, index) => (
+            <div className={styles.slide} key={index}>
+              <Card>{slide}</Card>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaceCarousel;
