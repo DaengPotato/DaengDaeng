f6e43a4dfb287bd02031cd7d396042ccf9db7d0b
haeun <kodms08@naver.com>
2023-09-19 16:54:08 +0900

    feat: 함수로 구분

--- backend/recommendFlask/venv/include/model/recomm_review_heart.py
+++ backend/recommendFlask/venv/include/model/recomm_review_heart.py
@@ -15,44 +15,17 @@ def review_heart_recomm(member_id):
         print(recom_place)
         return recom_place
 
-    # 찜이 된 모든 여행지 가지고 오기
-    data_review_heart = get_heart_place()
+    # 찜 유사도가 높은 사용자들 추리기
+    recom_people = recommend_people(member_id)
 
-    # pandas로 데이터 프레임으로 변환
-    dataframe_hearts=pd.DataFrame(data_review_heart,columns=['place_id','member_id', 'heart'])
-
-    # pivot table 만들기
-    ratings_matrix = dataframe_hearts.pivot_table(index="member_id", columns="place_id", values="heart")
-    # null값은 0으로 채우기
-    ratings_matrix.fillna(0, inplace=True)
-
-    # cosin으로 사용자 찜 유사도 구하기
-    item_sim = cosine_similarity(ratings_matrix, ratings_matrix)  # 협업필터링 아이템 기반
-
-    # 데이터 프레임 형태로 저장
-    item_sim_df = pd.DataFrame(item_sim, index=ratings_matrix.index, columns=ratings_matrix.index)
-
-    # 유사도가 높은 20명의 사용자 가지고 오기(본인 제외)
-    recom_people = item_sim_df[member_id].sort_values(ascending=False)
-    recom_people = recom_people[~recom_people.index.isin([member_id])][:5]
-
-    # 유사도가 높은 사람들이 좋은 리뷰를 남긴 여행지
-    place_by_recom_people = []
-
-    # 유사도가 높은 사람들이 별점을 5점 남긴 여행지 가지고 오기
-    for person in recom_people.index:
-        place_by_person_review = get_place_by_person_review(person)
-        place_by_person_review = [item[0] for item in place_by_person_review]
-        # 다 더해주기
-        place_by_recom_people.extend(place_by_person_review)
-
-    place_by_recom_people = list(set(place_by_recom_people))
+    # 유사도가 높은 사람들이 좋은 리뷰를 남긴 여행지들
+    recom_place = place_by_recom_people(recom_people)
 
     # 유사도 테이블
     similarity_table = review_heart_recomm_content()
 
-    # 추천 사용자이 높은 리뷰를 남긴 장소만 추리기
-    select_place = similarity_table[place_by_recom_people]
+    # 추천 사용자가 높은 리뷰를 남긴 장소만 추리기
+    select_place = similarity_table[recom_place]
 
     #사용자가 찜한 여행지와 추천 여행지(유사한 사용자가 좋은 리뷰를 남긴 여행지)와 유사도 계산
     top_similar_places = []
@@ -87,7 +60,46 @@ def review_heart_recomm(member_id):
 
     return result
 
-def review_heart_recomm_content(): # 콘텐츠 기반 유사도 측정
+
+def place_by_recom_people(recom_people): #유사도가 높은 사용자들이 높은 별점을 남긴 여행지들
+    recom_place = []
+    for person in recom_people.index:
+        # 별점을 5점 남긴 여행지 가지고 오기
+        place_by_person_review = get_place_by_person_review(person)
+        place_by_person_review = [item[0] for item in place_by_person_review]
+        # 다 더해주기
+        recom_place.extend(place_by_person_review)
+
+    recom_place = list(set(recom_place))
+    return recom_place
+
+
+def recommend_people(member_id): # 찜 유사도가 높은 사용자 가지고 오기
+    # 찜이 된 모든 여행지 가지고 오기
+    data_review_heart = get_heart_place()
+
+    # pandas로 데이터 프레임으로 변환
+    dataframe_hearts=pd.DataFrame(data_review_heart,columns=['place_id','member_id', 'heart'])
+
+    # pivot table 만들기
+    ratings_matrix = dataframe_hearts.pivot_table(index="member_id", columns="place_id", values="heart")
+    # null값은 0으로 채우기
+    ratings_matrix.fillna(0, inplace=True)
+
+    # cosin으로 사용자 찜 유사도 구하기
+    item_sim = cosine_similarity(ratings_matrix, ratings_matrix)  # 협업필터링 아이템 기반
+
+    # 데이터 프레임 형태로 저장
+    item_sim_df = pd.DataFrame(item_sim, index=ratings_matrix.index, columns=ratings_matrix.index)
+
+    # 유사도가 높은 20명의 사용자 가지고 오기(본인 제외)
+    recom_people = item_sim_df[member_id].sort_values(ascending=False)
+    recom_people = recom_people[~recom_people.index.isin([member_id])][:5]
+
+    return recom_people
+
+
+def review_heart_recomm_content(): # 콘텐츠 기반 유사도 테이블
     place_review_keyword = get_review_keyword()
 
     # pandas로 데이터 프레임으로 변환
