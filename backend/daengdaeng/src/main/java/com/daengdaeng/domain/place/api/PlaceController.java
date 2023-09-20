786e00e1334a6779e106d874ce82129cc316f47b
haeun <kodms08@naver.com>
2023-09-20 17:57:59 +0900

    feat: place 여행지 조회 구현중

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/api/PlaceController.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/api/PlaceController.java
@@ -1,7 +1,24 @@
 package com.daengdaeng.domain.place.api;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping("/place")
 public class PlaceController {
+
+	@GetMapping("/")
+	public ResponseEntity<String> getPlace(int category, String keyword, int page ) {
+		//
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 }
---
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/dto/response/FindAllPlaceResponse.java
@@ -0,0 +1,1 @@
+package com.daengdaeng.domain.place.dto.response;import java.util.ArrayList;import lombok.AllArgsConstructor;@AllArgsConstructorpublic class FindAllPlaceResponse {	private int page;	private ArrayList<PlaceResponse> placeResponseList;}
