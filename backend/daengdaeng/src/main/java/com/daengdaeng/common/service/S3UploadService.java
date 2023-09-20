b384420f81981495bdf1eb4911d91b29f455aa43
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-20 18:04:24 +0900

    S09P22A103-434 feat:  파일 업로드를 위한 Common 패키지 내 스켈레톤 파일 추가 및 수정(#8)

---
+++ backend/daengdaeng/src/main/java/com/daengdaeng/common/service/S3UploadService.java
@@ -0,0 +1,10 @@
+package com.daengdaeng.common.service;
+
+public interface S3UploadService {
+
+    // TODO: MultipartFile 사용할 수 있게 해서 구현 할 것. https://chanung.tistory.com/200    https://develop-writing.tistory.com/108   https://gaeggu.tistory.com/33    https://sennieworld.tistory.com/122    https://jsonobject.tistory.com/228
+
+//    public String uploadFileToS3(MultipartFile multipartFile, String filePath);
+
+
+}
