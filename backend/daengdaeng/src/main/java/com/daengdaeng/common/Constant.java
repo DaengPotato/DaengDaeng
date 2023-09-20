b5fb41ab8af260be38464a50f62f8e9d2d67aea6
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-20 18:04:24 +0900

    S09P22A103-434 feat:  파일 업로드를 위한 Common 패키지 내 스켈레톤 파일 추가 및 수정(#8)

--- backend/daengdaeng/src/main/java/com/daengdaeng/common/Constant.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/common/Constant.java
@@ -5,5 +5,4 @@ public class Constant {
     public static final String FILESERVER_DIRECTORY = "./fileserver/";
     public static final String DAENGFOURCUT_DIRECTORY = "./fileserver/daeng-four-cut";
     public static final String PROFILE_DIRECTORY = "./fileserver/profile";
-
 }
