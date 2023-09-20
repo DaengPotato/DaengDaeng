7c69d7afa28f9bedeb95f1d67819a82ed96c68f3
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-20 18:04:24 +0900

    S09P22A103-434 feat:  파일 업로드를 위한 Common 패키지 내 스켈레톤 파일 추가 및 수정(#8)

---
+++ backend/daengdaeng/src/main/java/com/daengdaeng/common/dto/ResponseMessage.java
@@ -0,0 +1,11 @@
+package com.daengdaeng.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// API 응답 메세지를 전하는 dto
+@AllArgsConstructor
+@Getter
+public class ResponseMessage {
+    private String message;
+}
---
+++ backend/daengdaeng/src/main/java/com/daengdaeng/config/S3Config.java
@@ -0,0 +1,30 @@
+package com.daengdaeng.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    //    @Value("${cloud.aws.credentials.access-key}")
+    private String iamAccessKey;
+
+    //    @Value("${cloud.aws.credentials.secret-key}")
+    private String iamSecretKey;
+
+    //    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(iamAccessKey, iamSecretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withRegion(region).enablePathStyleAccess()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
