2146a1a281542d3952a146011236d62e9d6928d4
jieun21124 <jieun21124@naver.com>
2023-09-22 04:24:34 +0900

    S09P22A103-450 feat: 반려동물 정보 등록 API 구현
    펫 정보 등록 구현
    Swagger 적용중

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/pet/service/PetService.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/pet/service/PetService.java
@@ -1,1 +1,1 @@
-package com.daengdaeng.domain.pet.service;public interface PetService {}
+package com.daengdaeng.domain.pet.service;import com.daengdaeng.domain.pet.dto.request.PetRequest;public interface PetService {    void addPet(PetRequest petRequest);}
