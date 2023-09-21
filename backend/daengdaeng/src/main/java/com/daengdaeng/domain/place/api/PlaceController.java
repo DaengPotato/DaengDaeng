fea459991f351273d483ad5ff157eb020255500f
haeun <kodms08@naver.com>
2023-09-21 17:49:20 +0900

    feat: 여행지 추천 api 구현 중

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/api/PlaceController.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/place/api/PlaceController.java
@@ -1,5 +1,7 @@
 package com.daengdaeng.domain.place.api;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.daengdaeng.domain.place.dto.response.FindPlaceByDogResponse;
+import com.daengdaeng.domain.place.dto.response.FindPlaceResponse;
 import com.daengdaeng.domain.place.service.PlaceService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,25 +19,25 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @RestController
 @Slf4j
-@RequestMapping("api/place")
+@RequestMapping("/place")
 public class PlaceController {
 
 	private final PlaceService placeService;
 
 	@GetMapping("/")
 	public ResponseEntity<String> getAllPlace(int category, String keyword, int page ) {
-		//
+
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
 
 	@GetMapping("/recommend/dog")
-	public ResponseEntity<String> getRecommendPlaceByPet() {
-		int memberId = 1;
-		// placeService
+	public ResponseEntity<List<FindPlaceByDogResponse>> getRecommendPlaceByPet() {
+		// String email = "1";
 
+		List<FindPlaceByDogResponse> findPlaceResponses = placeService.recommendPlaceByPet();
 
-		return new ResponseEntity<>(HttpStatus.OK);
+		return new ResponseEntity<>(findPlaceResponses,HttpStatus.OK);
 	}
 
 }
