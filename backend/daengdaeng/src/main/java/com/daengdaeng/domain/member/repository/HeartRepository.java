f6f8a78260db2fe08a49b46dbd09b8923cd09533
jieun21124 <jieun21124@naver.com>
2023-09-25 01:38:32 +0900

    S09P22A103-458 feat: 찜 추가 기능 구현
    찜 추가 기능 구현

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/member/repository/HeartRepository.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/member/repository/HeartRepository.java
@@ -1,17 +1,14 @@
 package com.daengdaeng.domain.member.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import com.daengdaeng.domain.member.domain.Heart;
-import com.daengdaeng.domain.member.domain.Member;
-import com.daengdaeng.domain.place.domain.Place;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HeartRepository extends JpaRepository< Heart,Integer> {
 
-
     boolean existsByMemberMemberIdAndPlacePlaceId(int memberId, int placeId);
+
     int countByPlacePlaceId(int placeId);
 
 }
--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/member/service/HeartService.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/member/service/HeartService.java
@@ -1,4 +1,7 @@
 package com.daengdaeng.domain.member.service;
 
 public interface HeartService {
+
+    void addHeart(int placeId);
+
 }
