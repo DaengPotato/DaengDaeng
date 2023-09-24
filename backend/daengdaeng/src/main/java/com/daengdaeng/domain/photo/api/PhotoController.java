60c14dd4294e033eb3686d501489b098990b7304
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-25 05:31:46 +0900

    S09P22A103-434 feat: photo api 5개 중 3개 구현. 중간저장 (#8)

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/photo/api/PhotoController.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/photo/api/PhotoController.java
@@ -2,13 +2,13 @@ package com.daengdaeng.domain.photo.api;
 
 import com.daengdaeng.domain.member.repository.MemberRepository;
 import com.daengdaeng.domain.photo.service.PhotoServiceImpl;
+import com.daengdaeng.global.util.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
@@ -17,36 +17,42 @@ import org.springframework.web.bind.annotation.RestController;
 public class PhotoController {
     private final PhotoServiceImpl photoService;
     private final MemberRepository memberRepository;
+    private final JwtTokenUtil jwtTokenUtil;
 
+    // TODO: 페이지네이션 처리 할 것
     @GetMapping
     // 전체 댕댕네컷 조회
-    public ResponseEntity<?> seeAllDaengs (){
+    public ResponseEntity<?> seeAllDaengs() {
         // 전체 댕댕네컷 정보 조회
 
-        return null;
+        return ResponseEntity.ok().body(photoService.findDaengsCutsAll());
     }
 
+    // TODO: 페이지네이션 처리 할 것
+    @GetMapping("/myPhoto")
     // 내 댕댕네컷 조회, 헤더 엑세스 토큰
-    public ResponseEntity<?> seeMyDaengs(){
+    public ResponseEntity<?> seeMyDaengs(@RequestHeader("Authorization") String token) {
         // 엑세스 토큰에서 유저 아이디 뽑기
-//        int memberId =
-        // 뽑은 유저 아이디로 댕댕네컷 조회
-//        photoService.findDaengsCutsByMemberId(memberId);
-     return null;
+        String userEmail = jwtTokenUtil.getUsername(token);
+        int memberId = memberRepository.findMemberByEmail(userEmail).getMemberId();
+        // 뽑은 유저 아이디로 댕댕네컷 조회해서 반환
+
+        return ResponseEntity.ok().body(photoService.findDaengsCutsByMemberId(memberId));
     }
 
     // 댕댕네컷 프레임 조회. 프레임은 s3에 있음
-    public ResponseEntity<?> seeFrames(){
+    public ResponseEntity<?> seeFrames() {
         // s3에서 전체 댕댕네컷 프레임 조회
-
-        return null;
+        return ResponseEntity.ok().body(photoService.findFrames());
     }
 
     // 댕댕네컷 s3 업로드 요청, 헤더 엑세스 토큰
-    public ResponseEntity<?> uploadDaengCut(){
-        // 헤더 엑세스 토큰에서 유저 아이디 뽑기
-
+    public ResponseEntity<?> uploadDaengCut(@RequestHeader("Authorization") String token, @RequestBody MultipartFile file) {
+        // 엑세스 토큰에서 유저 아이디 뽑기
+        String userEmail = jwtTokenUtil.getUsername(token);
+        int memberId = memberRepository.findMemberByEmail(userEmail).getMemberId();
         // s3에 유저 아이디로 업로드 요청
+
 
 
 
@@ -54,7 +60,7 @@ public class PhotoController {
     }
 
     // 댕댕네컷 업로드 결과 전송, 헤더 엑세스 토큰
-    public ResponseEntity<?> isSuccessedUploadDaengCut(){
+    public ResponseEntity<?> isSuccessedUploadDaengCut() {
         // 헤더 엑세스 토큰에서 유저 아이디 뽑기
 
         //
--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/photo/service/PhotoServiceImpl.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/photo/service/PhotoServiceImpl.java
@@ -1,13 +1,17 @@
 package com.daengdaeng.domain.photo.service;
 
-import com.daengdaeng.common.dto.ResponseMessage;
+import com.daengdaeng.common.service.S3ServiceImpl;
+import com.daengdaeng.domain.member.repository.MemberRepository;
 import com.daengdaeng.domain.photo.domain.Photo;
 import com.daengdaeng.domain.photo.repository.PhotoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Slf4j
@@ -16,43 +20,49 @@ import java.util.List;
 @Transactional
 public class PhotoServiceImpl implements PhotoService {
     private final PhotoRepository photoRepository;
+    private final MemberRepository memberRepository;
+    private final S3ServiceImpl S3Service;
 
     // 모든 댕댕네컷 정보 조회
     @Override
     public List<Photo> findDaengsCutsAll() {
         List<Photo> photoList = photoRepository.findAll();
-
         return photoList;
     }
 
     // memberId에 해당하는 댕댕네컷 정보 조회
     @Override
     public List<Photo> findDaengsCutsByMemberId(int memberId) {
-        List<Photo> photoList = photoRepository.
-        return null;
+        return photoRepository.findByMemberId(memberId);
     }
 
     // s3에서 프레임 정보 조회
     @Override
     public List<Photo> findFrames() {
+//        S3Service.
+
+
         return null;
     }
 
+    // TODO: 리스폰스메세지 dto에 담을 것?
     // 업로드한 댕댕네컷 파일에 대한 정보를 DB에 추가
     @Override
-    public ResponseMessage addDaengCut(String image, int memberId, int placeId) {
+    public String addDaengCut(String image, int memberId, int placeId) {
         return null;
     }
 
     // S3에 파일 업로드
     @Override
-    public ResponseMessage addFileAtS3(String fileName) {
-        return null;
+    public String addFileAtS3(String fileName, MultipartFile file) {
+        String newFileName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHH" + fileName));
+
+        return S3Service.uploadFile(file, "daengFourCut", newFileName);
     }
 
     // S3에서 파일명으로 조회
-    @Override
-    public ResponseMessage findFileAtS3(String fileName) {
-        return null;
-    }
+//    @Override
+//    public String findFileAtS3(String fileName) {
+//        return null;
+//    }
 }
