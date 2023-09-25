f3b597a1ef6f3dc576b84b1c731b6d6aa73bdc95
imhynni <omelas329@gmail.com>
2023-09-25 17:44:30 +0900

    fix: 버튼 클릭 시 카드 클릭으로 인식되는 것 수정

--- frontend/daengdaeng/src/app/mypets/myPets/PetCard/index.tsx
+++ frontend/daengdaeng/src/app/mypets/myPets/PetCard/index.tsx
@@ -21,17 +21,19 @@ const PetCard = ({ pet }: PetCardProps) => {
   };
 
   return (
-    <div className={styles.PetCard} onClick={handleEditClick}>
+    <div className={styles.PetCard}>
       <button className={styles.editIcon}>
         <EditIcon fill={'#000000'} width={20} height={20} />
       </button>
-      <div className={styles.petName}>{pet.name}</div>
-      <div className={styles.petMbti}>{pet.mbtiId}</div>
-      <div className={styles.petImg}>
-        <Image src={pet.image} alt="pet img" fill={true} />
-      </div>
-      <div className={styles.petDetail}>
-        {pet.birth} | {pet.gender} | {pet.weight}
+      <div onClick={handleEditClick}>
+        <div className={styles.petName}>{pet.name}</div>
+        <div className={styles.petMbti}>{pet.mbtiId}</div>
+        <div className={styles.petImg}>
+          <Image src={pet.image} alt="pet img" fill={true} />
+        </div>
+        <div className={styles.petDetail}>
+          {pet.birth} | {pet.gender} | {pet.weight}
+        </div>
       </div>
       <div className={styles.petPlaceBtn}>
         <Button size="small" backgroundColor="orange" icon={true}>
