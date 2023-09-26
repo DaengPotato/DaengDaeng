19196e25df0173936474ab98218f4dfc14521760
김혜진 <zlagg23@gmail.com>
2023-09-26 17:18:11 +0900

    fix : 충돌 해결

--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/review/repository/ReviewKeywordRepository.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/review/repository/ReviewKeywordRepository.java
@@ -1,14 +1,12 @@
 package com.daengdaeng.domain.review.repository;
 
 import com.daengdaeng.domain.review.domain.Review;
-import com.daengdaeng.domain.review.domain.ReviewPet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.daengdaeng.domain.review.domain.ReviewKeyword;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ReviewKeywordRepository extends JpaRepository<ReviewKeyword, Integer> {
--- backend/daengdaeng/src/main/java/com/daengdaeng/domain/review/repository/ReviewPetRepository.java
+++ backend/daengdaeng/src/main/java/com/daengdaeng/domain/review/repository/ReviewPetRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface ReviewPetRepository extends JpaRepository<ReviewPet, ReviewPetId> {
