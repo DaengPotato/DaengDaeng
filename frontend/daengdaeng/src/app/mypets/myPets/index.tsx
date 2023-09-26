81ee04d4f2366d5b06b95b096b1ca1257164dbeb
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-26 17:44:41 +0900

    S09P22A103-434 리베이스 진행을 위한 저장 (#8)

--- frontend/daengdaeng/src/app/mypets/myPets/index.tsx
+++ frontend/daengdaeng/src/app/mypets/myPets/index.tsx
@@ -1,21 +1,33 @@
 'use client';
 
 import React from 'react';
+import { useState } from 'react';
 
 import AddPetButton from './AddPetButton';
 import styles from './index.module.scss';
 import PetCarousel from './PetCarousel';
+import PetRegistForm from './PetRegistForm';
 
 import type { PetDetail } from '@/src/types/pet';
+import type { Place } from '@/src/types/place';
+
+import Modal from '@/src/components/common/Modal';
+import PlaceCarousel from '@/src/components/PlaceCarousel';
 
 type MyPetsProps = {
   pets: PetDetail[];
+  places: Place[];
 };
 
-const MyPets = ({ pets }: MyPetsProps) => {
+const MyPets = ({ pets, places }: MyPetsProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   // TODO: 내 강아지 데이터 fetch
 
-  const handleClickAddPet = () => {};
+  const handleClickAddPet = () => {
+    // TODO: 모달 열기
+    setIsOpen(true);
+  };
 
   return (
     <div>
@@ -30,6 +42,21 @@ const MyPets = ({ pets }: MyPetsProps) => {
           options={{ dragFree: true, containScroll: 'trimSnaps' }}
         />
       </div>
+      <div className={styles.myPlaceList}>
+        <div className={styles.header}>
+          <span className={styles.title}>내가 찜한 여행지</span>
+          <button className={styles.moreBtn}>더보기</button>
+        </div>
+        <PlaceCarousel
+          places={places}
+          options={{ dragFree: true, containScroll: 'trimSnaps' }}
+        />
+      </div>
+      {isOpen && (
+        <Modal setIsOpen={setIsOpen}>
+          <PetRegistForm setIsOpen={setIsOpen} />
+        </Modal>
+      )}
     </div>
   );
 };
