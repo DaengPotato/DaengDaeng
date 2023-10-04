729cd8565e22340a27a37279f4e550989b1b5b23
imhynni <omelas329@gmail.com>
2023-10-04 17:39:33 +0900

    fix: next.config.js 이미지 경로 추가

--- frontend/daengdaeng/next.config.js
+++ frontend/daengdaeng/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['s3.ap-northeast-2.amazonaws.com'],
+    domains: ['s3.ap-northeast-2.amazonaws.com', 'search.pstatic.net'],
   },
   eslint: {
     ignoreDuringBuilds: true,
