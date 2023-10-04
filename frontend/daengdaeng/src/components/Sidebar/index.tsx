1857f44491bc24da402796335e1a9fe1e1751e15
dkgusdkfk <dkgusdkfk@gmail.com>
2023-10-04 17:52:43 +0900

    S09P22A103-536 feat: 리뷰 화면 구현 리뷰 화면 구현

--- frontend/daengdaeng/src/components/Sidebar/index.tsx
+++ frontend/daengdaeng/src/components/Sidebar/index.tsx
@@ -22,6 +22,36 @@ import {
   removeUserInfo,
 } from '@/src/hooks/useLocalStorage';
 
+import styles from './index.module.scss';
+import Button from '../common/Button';
+
+const menuItems = [
+  {
+    label: '나의 강아지',
+    href: '/mypets',
+  },
+  {
+    label: '댕BTI 검사',
+    href: '/mbti',
+  },
+  {
+    label: '여행지 추천',
+    href: '/placerecommendation',
+  },
+  {
+    label: '여행지 검색',
+    href: '/placesearch',
+  },
+  {
+    label: '댕댕네컷',
+    href: '/daengphoto',
+  },
+  {
+    label: '리뷰',
+    href: '/placereview',
+  },
+];
+
 const Sidebar = ({
   isMenuOpen,
   setIsMenuOpen,
