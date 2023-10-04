384b86f0bf0944a4c1c312ba3c0e86a56d76c4ad
dkgusdkfk <dkgusdkfk@gmail.com>
2023-10-04 09:41:39 +0900

    S09P22A103-505 feat: Bottom Sheet Bottom Sheet

---
+++ frontend/daengdaeng/src/components/BottomSheet/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Sheet from 'react-modal-sheet';
+
+import styles from './index.module.scss';
+
+type BottomSheetProps = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const BottomSheet = ({ children, isOpen, setIsOpen }: BottomSheetProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <Sheet
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        className={styles.sheet}
+      >
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{children}</Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
+    </div>
+  );
+};
+
+export default BottomSheet;
