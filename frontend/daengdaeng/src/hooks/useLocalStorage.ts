6bff4f1e3af4c66eb1864b50277be36222554927
imhynni <omelas329@gmail.com>
2023-10-05 14:53:01 +0900

    fix: localstorage key 변수 참조 에러 수정

--- frontend/daengdaeng/src/hooks/useLocalStorage.ts
+++ frontend/daengdaeng/src/hooks/useLocalStorage.ts
@@ -1,21 +1,17 @@
 import { findUserInfo } from '../apis/api/member';
-import {
-  USER_INFO_LOCAL_STORAGE_KEY,
-  USER_LOCAL_STORAGE_KEY,
-} from '../constants/localStorageKey';
 
 import type { UserInfo } from '../types/member';
 
 export async function saveUser(token: string): Promise<void> {
   if (typeof window !== 'undefined') {
-    localStorage.setItem(USER_LOCAL_STORAGE_KEY, token);
+    localStorage.setItem('USER', token);
     saveUserInfo(await findUserInfo(token));
   }
 }
 
 export function getUser(): string | undefined {
   if (typeof window !== 'undefined') {
-    const accessToken = localStorage.getItem(USER_LOCAL_STORAGE_KEY);
+    const accessToken = localStorage.getItem('USER');
     return accessToken as string;
   }
   return undefined;
@@ -23,20 +19,20 @@ export function getUser(): string | undefined {
 
 export function removeUser(): void {
   if (typeof window !== 'undefined') {
-    localStorage.removeItem(USER_LOCAL_STORAGE_KEY);
-    localStorage.removeItem(USER_INFO_LOCAL_STORAGE_KEY);
+    localStorage.removeItem('USER');
+    localStorage.removeItem('USER_INFO');
   }
 }
 
 export function saveUserInfo(userInfo: UserInfo): void {
   if (typeof window !== 'undefined') {
-    localStorage.setItem(USER_INFO_LOCAL_STORAGE_KEY, JSON.stringify(userInfo));
+    localStorage.setItem('USER_INFO', JSON.stringify(userInfo));
   }
 }
 
 export function getUserInfo(): UserInfo | undefined {
   if (typeof window !== 'undefined') {
-    const userInfo = localStorage.getItem(USER_INFO_LOCAL_STORAGE_KEY);
+    const userInfo = localStorage.getItem('USER_INFO');
     return JSON.parse(userInfo as string);
   }
   return undefined;
@@ -44,6 +40,6 @@ export function getUserInfo(): UserInfo | undefined {
 
 export function removeUserInfo(): void {
   if (typeof window !== 'undefined') {
-    localStorage.removeItem(USER_INFO_LOCAL_STORAGE_KEY);
+    localStorage.removeItem('USER_INFO');
   }
 }
