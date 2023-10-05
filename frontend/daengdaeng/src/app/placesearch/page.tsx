5d3ac1271fcf1a581ba6759e7d58c4ead9bcdf42
dkgusdkfk <dkgusdkfk@gmail.com>
2023-10-06 03:44:37 +0900

    S09P22A103-540 fix: 사이드바 조건부 사이드바 조건부

--- frontend/daengdaeng/src/app/placesearch/page.tsx
+++ frontend/daengdaeng/src/app/placesearch/page.tsx
@@ -2,12 +2,31 @@
 import { useEffect, useState } from 'react';
 
 import PlaceSearch from '@/src/app/placesearch/PlaceSearch';
-import useFetcher from '@/src/hooks/useFetcher';
 
-import type { Category } from '@/src/types/category';
+const fetchCategories = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/place/category`,
+    {
+      method: 'GET',
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error('카테고리 조회 실패');
+  }
+  const data = JSON.parse(await response.text());
+
+  return data;
+};
 
 const PlaceSearchPage = () => {
-  const { data: categories } = useFetcher<Category[]>(`/place/category`);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setCategories(await fetchCategories());
+    })();
+  }, []);
 
   return <PlaceSearch categories={categories} />;
 };
